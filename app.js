97c58c6254f265a641895ce924b5996daada5d41
Timothy John Manas <timothy.john.manas@gmail.com>
2019-09-11 20:55:10 -0400

    Created Router

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require ("express")     // We make this const because we dont want this to change ever. 
+const express = require("express")     // We make this const because we dont want this to change ever. 
 const app = express()
+const router = require("./router.js")
+
 
 // We need to tell express where to find our "views"
-app.use(express.static("public")) // Make the 
-app.set("views", "views")       // The second argument is the name of the file(In this case it is views)
+app.use(express.static("./public/")) // location of the static assets
+
+// We nee to set the views location & Engine Type
+app.set("views", "./views/")       // The second argument is the name of the file(In this case it is views)
 app.set("view engine", "ejs")   // We need to define the view engine
 
-
-// Scenario: Guess view Home Page Landing 
-app.get("/", function(req, res) {
-    res.render("home-guest")        // We use Render because our Engine 
-})
+// Route our requests
+app.use("/", router)        // Routes our request
 
 app.listen(3000)
---
+++ router.js
@@ -0,0 +1,12 @@
+ const express = require("express")
+ const router = express.Router()
+
+
+ router.get("/", function(req, res) {
+    res.render("home-guest")
+ })
+
+
+ 
+
+ module.exports = router            // router is kinda like a new mini express Application
