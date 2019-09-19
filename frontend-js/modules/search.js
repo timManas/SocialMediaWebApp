d7b6aeeb3754b7d496f0bf77eaf42e15d6f25f09
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-09-19 19:32:46 -0400

    Modified the search result HTML so it dynamically displays the result. Also can click on the post to open it

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -71,24 +71,17 @@ export default class Search {
     renderResultsHTML(posts) {
       if (posts.length) {
         this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
-        <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-          <span class="text-muted small">by barksalot on 0/14/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-          <span class="text-muted small">by brad on 0/12/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-          <span class="text-muted small">by barksalot on 0/14/2019</span>
-        </a>
-        <a href="#" class="list-group-item list-group-item-action">
-          <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-          <span class="text-muted small">by brad on 0/12/2019</span>
-        </a>
+        <div class="list-group-item active"><strong>Search Results</strong> (${posts.length} post found)</div>
+        ${posts.map(post => {
+          let postDate = new Date(post.createdDate)
+          return `
+          <a href="/post/${post._id}" class="list-group-item list-group-item-action">
+            <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${post.title}</strong>
+            <span class="text-muted small">by ${post.author.username} on ${postDate.getDay()}/${postDate.getMonth()}/${postDate.getFullYear()}</span>
+          </a>`
+        
+        }).join("")}
+        
       </div>`
       } else {
         this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">No Result Found</p>`
