9e0915561c10c8487aeaf824b440fe8e9f05b799
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-09-22 19:41:15 -0400

    Created the GUI HTML template for CHAT. Able to open and close chat window

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,4 +1,10 @@
 import Search from './modules/search'       // Why not use requires() here ?
+import Chat from "./modules/chat"
+
+// Leverage Chat IF the chat wrapper exists - i.e If user is logged
+if (document.querySelector("#chat-wrapper")) {
+    new Chat()
+}   
 
 // We start Search only when the search icon is there 
 if (document.querySelector(".header-search-icon")) {
---
+++ frontend-js/modules/chat.js
@@ -0,0 +1,61 @@
+export default class Chat {
+
+    constructor() {
+        this.openedYet = false                                      // Need to check if window opened so we can establish a DB connection
+        this.chatWrapper = document.querySelector("#chat-wrapper")      // When you need to select a specifc Element # for id   . is for class
+        this.openIcon = document.querySelector(".header-chat-icon")     // Icon for opening the chat
+        
+        this.injectHTML()
+        this.closeIcon = document.querySelector(".chat-title-bar-close") // Icon for closing the chat inside the chat window
+        this.events()
+    }
+
+    // Events
+    events() {
+
+        /**
+          Remember the reason we use () => function is because the arrow function will not
+          modify the this keyword 
+
+          Hence use:
+          => when you want to refer to the object
+          function(a,b) when you want to refer to the element getting clicked on 
+
+         */
+
+
+        this.openIcon.addEventListener("click", () => this.showChat())
+        this.closeIcon.addEventListener("click", () => this.hideChat())
+    }
+
+    // Methods
+
+    showChat() {
+        if(!this.openedYet) {
+            this.openConnection()
+        }
+
+        this.openedYet = true
+        this.chatWrapper.classList.add("chat--visible")
+    }
+
+    hideChat() {
+        this.chatWrapper.classList.remove("chat--visible")
+    }
+
+    injectHTML() {
+        this.chatWrapper.innerHTML = `
+        <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div> 
+        <div id="chat" class="chat-log"></div>
+        <form id="chatForm" class="chat-form border-top">
+            <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
+        </form>
+        `
+    }
+
+    openConnection() {
+        alert("Opened connection")
+    }
+
+
+}
